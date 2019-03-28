bc297b82d8bc37dfcb76c1c11ce7009f79165999
Sverre Johann Bjørke <Sverre.Bjorke@spk.no>
2019-03-28 15:15:26 +0100

    started on json-stuffs

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,9 @@
 import Browser
-import Html exposing (Html, Attribute, div, input, text, h1, h2)
+import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (Decoder, field, string)
 
 
 
@@ -9,21 +11,27 @@ import Html.Events exposing (onInput)
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
 
 
 
 -- MODEL
 
 
-type alias Model =
-  { content : String
-  }
+type Model
+  = Failure
+  | Loading
+  | Success String
 
 
-init : Model
-init =
-  { content = "" }
+init : () -> (Model, Cmd Msg)
+init _ =
+  (Loading, getRandomCatGif)
 
 
 
@@ -31,14 +39,32 @@ init =
 
 
 type Msg
-  = Change String
+  = MorePlease
+  | GotGif (Result Http.Error String)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Change newContent ->
-      { model | content = newContent }
+    MorePlease ->
+      (Loading, getRandomCatGif)
+
+    GotGif result ->
+      case result of
+        Ok url ->
+          (Success url, Cmd.none)
+
+        Err _ ->
+          (Failure, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
 
 
 
@@ -49,8 +75,40 @@ view : Model -> Html Msg
 view model =
   div []
     [ 
-        div [] [text ("Header")]
-      , div [] [text ("Main")]
-      , div [] [text ("Sidebar")]
-      , div [] [text ("Footer")]
+      div [] [ h1 [] [ text "Header" ] ]
+    , div [] [ h2 [] [ text "Content" ], viewGif model ]
+    , div [] [ h2 [] [text "Footer"]]
     ]
+
+
+viewGif : Model -> Html Msg
+viewGif model =
+  case model of
+    Failure ->
+      div []
+        [ text "Kunne ikke laste blogg."
+        , button [ onClick MorePlease ] [ text "Try Again!" ]
+        ]
+
+    Loading ->
+      text "Loading blogposts"
+
+    Success url ->
+      div []
+        [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
+        , img [ src url ] []
+        ]
+
+-- HTTP
+
+getRandomCatGif : Cmd Msg
+getRandomCatGif =
+  Http.get
+    { url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
+    , expect = Http.expectJson GotGif gifDecoder
+    }
+
+
+gifDecoder : Decoder String
+gifDecoder =
+  field "data" (field "image_url" string)
