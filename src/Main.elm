4f5263476b974eea074b31811b95ea2338b668e9
Sverre Johann Bjørke <Sverre.Bjorke@spk.no>
2019-03-29 08:57:24 +0100

    elm-format

--- src/Main.elm
+++ src/Main.elm
@@ -1,48 +1,55 @@
+module Main exposing (BlogPost, Model(..), Msg(..), backendUrl, blogDecoder, blogPostDecoder, getBlogList, init, main, renderList, subscriptions, update, view, viewBlogList, viewSpinner)
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
-import Json.Decode as Decode exposing (Decoder, field, string, list)
+import Json.Decode as Decode exposing (Decoder, field, list, string)
 import Loading
-  exposing
-      ( LoaderType(..)
-      , defaultConfig
-      , render
-      )
+    exposing
+        ( LoaderType(..)
+        , defaultConfig
+        , render
+        )
 
 
 
 -- MAIN
 
-backendUrl = "https://www.googleapis.com/drive/v3/files?q=%271pOJUeCNvFbHEbPxM4FkL8fePbZ4Ct_Ak%27%20in%20parents&key=AIzaSyDOw0EmUh-dNvg3qXvJ7ewkZNJgTIxtK_o"
+
+backendUrl =
+    "https://www.googleapis.com/drive/v3/files?q=%271pOJUeCNvFbHEbPxM4FkL8fePbZ4Ct_Ak%27%20in%20parents&key=AIzaSyDOw0EmUh-dNvg3qXvJ7ewkZNJgTIxtK_o"
+
 
 main =
-  Browser.element
-    { init = init
-    , update = update
-    , subscriptions = subscriptions
-    , view = view
-    }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
 
 
 -- MODEL
 
-type alias BlogPost
-  = { name : String,
-      id : String}
+
+type alias BlogPost =
+    { name : String
+    , id : String
+    }
 
 
 type Model
-  = Failure Http.Error
-  | Loading
-  | Success (List BlogPost)
+    = Failure Http.Error
+    | Loading
+    | Success (List BlogPost)
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
 init _ =
-  (Loading, getBlogList)
+    ( Loading, getBlogList )
 
 
 
@@ -50,23 +57,23 @@ init _ =
 
 
 type Msg
-  = FetchBlogposts
-  | GotBlogList (Result Http.Error (List BlogPost))
+    = FetchBlogposts
+    | GotBlogList (Result Http.Error (List BlogPost))
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    FetchBlogposts ->
-      (Loading, getBlogList)
+    case msg of
+        FetchBlogposts ->
+            ( Loading, getBlogList )
 
-    GotBlogList result ->
-      case result of
-        Ok url ->
-          (Success url, Cmd.none)
+        GotBlogList result ->
+            case result of
+                Ok url ->
+                    ( Success url, Cmd.none )
 
-        Err err ->
-          (Failure err, Cmd.none)
+                Err err ->
+                    ( Failure err, Cmd.none )
 
 
 
@@ -75,60 +82,70 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
+
+
 
 -- VIEW
+
 
 renderList : List BlogPost -> Html msg
 renderList lst =
     ul []
         (List.map (\l -> li [] [ text l.name ]) lst)
 
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ 
-      div [] [ h1 [] [ text "Clever Blog Title" ] ]
-    , div [] [ h2 [] [ text "Interesting ramblings" ], viewBlogList model ]
-    , div [] [ h2 [] [text "Footer? Footer."]]
-    ]
+    div []
+        [ div [] [ h1 [] [ text "Clever Blog Title" ] ]
+        , div [] [ h2 [] [ text "Interesting ramblings" ], viewBlogList model ]
+        , div [] [ h2 [] [ text "Footer? Footer." ] ]
+        ]
+
 
 viewSpinner : Html Msg
 viewSpinner =
-  div [ ]
-  [ Loading.render
-    Spinner
-    { defaultConfig | color = "#000000" }
-    Loading.On
-  ]    
+    div []
+        [ Loading.render
+            Spinner
+            { defaultConfig | color = "#000000" }
+            Loading.On
+        ]
 
 
 viewBlogList : Model -> Html Msg
 viewBlogList model =
-  case model of
-    Failure err ->
-      div []
-        [ text "Unable to load blogposts"
-        , button [ onClick FetchBlogposts ] [ text "Try Again!" ]
-        ]
+    case model of
+        Failure err ->
+            div []
+                [ text "Unable to load blogposts"
+                , button [ onClick FetchBlogposts ] [ text "Try Again!" ]
+                ]
 
-    Loading ->
-      viewSpinner
+        Loading ->
+            viewSpinner
 
-    Success blogPosts ->
-      renderList blogPosts
+        Success blogPosts ->
+            renderList blogPosts
+
+
 
 -- HTTP
 
+
 getBlogList : Cmd Msg
 getBlogList =
-  Http.get
-    { url = backendUrl
-    , expect = Http.expectJson GotBlogList blogDecoder
-    }
+    Http.get
+        { url = backendUrl
+        , expect = Http.expectJson GotBlogList blogDecoder
+        }
+
 
 blogDecoder : Decoder (List BlogPost)
-blogDecoder = Decode.at ["files"] (Decode.list blogPostDecoder)
+blogDecoder =
+    Decode.at [ "files" ] (Decode.list blogPostDecoder)
+
 
 blogPostDecoder : Decoder BlogPost
 blogPostDecoder =
