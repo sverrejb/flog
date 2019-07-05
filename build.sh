a3f462a3d801fd35ecc48567e1d0e564345cefce
Sverre Johann Bjørke <Sverre.Bjorke@spk.no>
2019-07-05 13:13:48 +0200

    updated build

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ js="elm.js"
 min="main.js"
 
 npm -g install elm
-elm make --optimize --output=$js $@
+elm make --optimize --output=$js src/Main.elm
 
 uglifyjs $js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output=dist/static/$min
 
